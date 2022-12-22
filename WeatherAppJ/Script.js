41e52cd3e72c1bff0544458b438526e0b722882a
jayhunter123 <78044589+jayhunter123@users.noreply.github.com>
2022-12-22 19:40:37 +0000

    Add files via upload

--- WeatherAppJ/Script.js
+++ WeatherAppJ/Script.js
@@ -5,9 +5,40 @@ let weather = {
             "https://api.openweathermap.org/data/2.5/weather?q=" 
             + city 
             + "&units=metric&appid=" 
-            + this.appKey
+            + this.apiKey
         )
         .then((response) => response.json())
         .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        // console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
-}
+};
+// waits for search entry
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+});
+// waits for enter key
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+// default location
+weather.fetchWeather("London");
